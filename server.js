7e43ae98abc48b6a3eceafb018a8448c7e1e3ed2
RollyLumibao <blatch.lumibao@gmail.com>
2016-07-07 18:54:35 +0800

    commiting

--- server.js
+++ server.js
@@ -17,10 +17,19 @@ io.sockets.on('connection',function(socket){
 	connections.push(socket);
 	console.log('Connected: %s sockets connected', connections.length);
 
-	//disconnect
+//disconnect
 	socket.on('disconnect',function(data){
 		connections.splice(connections.indexOf(socket),1);
 		console.log('Disconnected: %s sockets connected',connections.length); 
+	});
+	//send message
+	socket.on('send message',function(data){
+		console.log(data)
+		io.sockets.emit('new message',{msg: data, user: socket.username });
+	});
+	//new users
+	socket.on('new user', function(data){
+		socket.username = data;
 	});
 
 });
