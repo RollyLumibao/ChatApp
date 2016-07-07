53942ae84e3f0c4915d52ceaf99e490e1b92c60f
RollyLumibao <blatch.lumibao@gmail.com>
2016-07-07 19:55:41 +0800

    update

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require ('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-users = [];
 connections = [];
 
 server.listen(process.env.PORT || 3000);
