d63bb58a290b1f47f55a383a3c1215eca4442536
RollyLumibao <rollylumibao@yahoo.com>
2016-07-11 12:15:49 +0800

    updating

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 connections = [];
+users=[];
 
 server.listen(process.env.PORT || 3000);
 console.log('server running...');
@@ -18,6 +19,8 @@ io.sockets.on('connection',function(socket){
 
 //disconnect
 	socket.on('disconnect',function(data){
+		users.splice(users.indexOf(socket.username),1)
+		updateUsernames();
 		connections.splice(connections.indexOf(socket),1);
 		console.log('Disconnected: %s sockets connected',connections.length); 
 	});
@@ -27,8 +30,19 @@ io.sockets.on('connection',function(socket){
 		io.sockets.emit('new message',{msg: data, user: socket.username });
 	});
 	//new users
-	socket.on('new user', function(data){
+	socket.on('new user',function(data){
 		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
 	});
-
+	function updateUsernames(){
+		io.sockets.emit('get users',users);
+	}
+	/*//PM USERS
+		users[socket.username] = socket.id;
+		io.sockets.connected[users[USER_NAME]]
+  		io.sockets.emit('private', {
+       msg:'private message for user with name '+ USER_NAME
+  	 });*/
+	
 });
